27f3c8a35154319fc5d70b1f6b96d87a795b76e1
imsamurai <imsamurai@10.0.0.118>
2013-10-30 11:32:01 +0200

    add to travis

---
+++ Test/Case/AllDynamicDatabaseConfigTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Author: imsamurai <im.samuray@gmail.com>
+ * Date: 30.10.2013
+ * Time: 11:00:00
+ *
+ */
+class AllDynamicDatabaseConfigTest extends PHPUnit_Framework_TestSuite {
+
+	/**
+	 * Suite define the tests for this suite
+	 *
+	 * @return void
+	 */
+	public static function suite() {
+		$suite = new CakeTestSuite('All DynamicDatabase Config Tests');
+
+		$path = App::pluginPath('DynamicDatabaseConfig') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+		return $suite;
+	}
+
+}
